75c76598bb3be9818701bac3d146ea3694cd3dd0
hinto <hinto@rambler.ru>
2022-11-11 21:23:34 +0700

    Реализует открытие/закрытие модального окна

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
 const modal = document.querySelector('.modal-container');
-const popupButton = document.querySelector('.navigation-button-link');
+const popupOpenButton = document.querySelector('.navigation-button-link');
+const popupCloseButton = document.querySelector('.modal-close-button');
 
-popupButton.addEventListener('click', (evt) => {
+popupOpenButton.addEventListener('click', (evt) => {
   evt.preventDefault();
   modal.classList.remove('modal-disabled');
+});
+
+popupCloseButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.classList.add('modal-disabled');
 });
